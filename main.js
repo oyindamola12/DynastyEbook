e2ed332fbbaf097efee745e8e01f9480c31897fd
oyindamola12 <72942392+oyindamola12@users.noreply.github.com>
2024-05-10 13:24:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -36,8 +36,8 @@ return false;
 
  var handler = PaystackPop.setup({
 
-key:'pk_live_b3635cadaea0a7a02518aed3dd9e8ac3d64a83cc',
-    //  key:'pk_test_733942352847369db55d32dc2b83d44db6b47fb1',
+//key:'pk_live_b3635cadaea0a7a02518aed3dd9e8ac3d64a83cc',
+    key:'pk_test_733942352847369db55d32dc2b83d44db6b47fb1',
       email:email,
       amount: 3500 * 100,
       ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
